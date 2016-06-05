ed289aeaa89b7817ced801b7465209349af5209f
pwrobel <wrobel1989@gmail.com>
2016-06-05 23:26:12 +0200

    Add some sample Java-computed backend for the image processing.

---
+++ app/src/main/java/com/pwrobel/darkcam1/CPUJavaBackend.java
@@ -0,0 +1,47 @@
+package com.pwrobel.darkcam1;
+
+/**
+ * Created by pwrobel on 05.06.16.
+ */
+public class CPUJavaBackend implements StaticPhotoRenderBackend {
+
+    private int width;
+    private int height;
+    private int bpp;
+    private int[] buf;
+
+    //initialize CPU renderer backend
+    public void Init(){
+
+    };
+
+    //0 = backend is processing the frame, >0 - denotes some reason of why it's busy
+    public int getBackendBusy(){
+        return 0;
+    };
+
+    //set camera full resolution data
+    public void setImgBuffer(int[] buff, int width, int height, int bpp){
+        if(width*height*bpp != this.width*this.height*this.bpp)
+            this.buf = new int[width*height*bpp];
+        this.width = width;
+        this.height = height;
+        this.bpp = bpp;
+        for (int j = 0; j < height; j++)
+            for (int i = 0; i < width; i++){
+                for (int k = 0; k < bpp; k++)
+	                this.buf[bpp*(j*width + i) + k] = buff[bpp*(j*width + i) + k];
+            }
+    };
+
+    //apply shader effect in CPU on the buffer
+    public int processBuffer(){
+        return 1;
+    };
+
+    //save the processed buffer on the disk
+    public int saveBufferToDisk(){
+        return 1;
+    };
+
+}
