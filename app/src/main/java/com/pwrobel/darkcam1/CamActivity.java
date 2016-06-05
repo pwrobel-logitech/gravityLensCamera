944e48993f39f10338568322cab4ef8c1342d2c6
pwrobel <wrobel1989@gmail.com>
2016-06-05 23:50:48 +0200

    Add button action.

--- app/src/main/java/com/pwrobel/darkcam1/CamActivity.java
+++ app/src/main/java/com/pwrobel/darkcam1/CamActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
+import android.content.Context;
 
 public class CamActivity extends ActionBarActivity {
 
@@ -19,6 +23,7 @@ public class CamActivity extends ActionBarActivity {
 
         setContentView(R.layout.activity_cam);
         mRenderer = (CameraRenderer)findViewById(R.id.renderer_view);
+        this.addListeners();
     }
 
     @Override
@@ -39,5 +44,22 @@ public class CamActivity extends ActionBarActivity {
     public void onResume(){
         super.onResume();
         mRenderer.onResume();
+    }
+
+    private void addListeners(){
+
+        Button buttonOne = (Button) findViewById(R.id.button);
+        buttonOne.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+                Context context = getApplicationContext();
+                CharSequence text = "Picture order dispatched!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
     }
 }
