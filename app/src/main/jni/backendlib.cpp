2d34600003f06ef0096a890ebc9381f9d0a4d970
pwrobel <wrobel1989@gmail.com>
2016-09-03 18:40:53 +0200

    Initial native processing template.

--- app/src/main/jni/backendlib.cpp
+++ app/src/main/jni/backendlib.cpp
@@ -1,14 +1,22 @@
 #include "backend.h"
+#include "math.h"
 
+#include <pthread.h>
 
 JNIEXPORT jdouble JNICALL Java_com_pwrobel_darkcam1_NativeCPUBackend_internal_1numerical_1test
-  (JNIEnv *, jobject, jdouble){
-    return 6.0 * 4.5;
+  (JNIEnv *, jobject, jdouble in){
+    return M_PI + sin(in);
   };
 
 
 JNIEXPORT void JNICALL Java_com_pwrobel_darkcam1_NativeCPUBackend_process_1buffer
-  (JNIEnv *g_vm, jobject, jintArray, jintArray, jint, jint, jdouble, jdouble, jint)
+  (JNIEnv *env, jobject, jintArray jinput, jintArray joutput, jint w, jint h, jdouble phys_ratio, jdouble fovX_deg, jint nThr)
   {
+    const jint *in_buff = env->GetIntArrayElements(jinput , 0);
+    jint *out_buff = env->GetIntArrayElements(joutput , 0);
 
+
+
+    env->ReleaseIntArrayElements(jinput, (jint*)in_buff, 0);
+    env->ReleaseIntArrayElements(joutput, out_buff, 0);
   };
