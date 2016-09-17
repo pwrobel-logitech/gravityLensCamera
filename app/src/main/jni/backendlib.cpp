0b616bd93a7ea2de36d04481aff0e4e8a8ee1d7b
pwrobel <wrobel1989@gmail.com>
2016-09-18 01:04:29 +0200

    fix logs error.

--- app/src/main/jni/backendlib.cpp
+++ app/src/main/jni/backendlib.cpp
@@ -89,8 +89,8 @@ void *perform_subimage_transform(void *args) {
     int loop_y_start = (int)(((float)info->num_y/(float)info->div_y)*info->h);
     int loop_y_end = (int)(((float)(info->num_y+1)/(float)info->div_y)*info->h);
     //__android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "raaaatio %f, rx %d, ry %d, loopingx: q%d,%dq, loopingy: q%d,%dq", info->phys_ratio,
-    info->num_x, info->num_y, loop_x_start,loop_x_end,
-                        loop_y_start,loop_y_end);
+    //info->num_x, info->num_y, loop_x_start,loop_x_end,
+     //                   loop_y_start,loop_y_end);
     for (int j = loop_y_start; j < loop_y_end; j++) //y
         for (int i = loop_x_start; i < loop_x_end; i++){ //x
             process_pixel(i, j, info->w, info->h,
